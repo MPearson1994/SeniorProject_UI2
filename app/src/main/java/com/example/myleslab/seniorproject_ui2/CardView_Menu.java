4493ac4df25814c401a8ba9f5b5c5d88964ca062
lab <myles.lab@Tnstate.edu>
2017-10-05 16:14:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myleslab/seniorproject_ui2/CardView_Menu.java
@@ -0,0 +1,14 @@
+package com.example.myleslab.seniorproject_ui2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CardView_Menu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_view__menu);
+
+    }
+}
