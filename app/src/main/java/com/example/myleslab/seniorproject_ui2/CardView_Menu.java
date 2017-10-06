86d989f633ce86a639b61f2aea2021ce34ae6a71
Myles.lab <MPearso2@my.tnstate.edu>
2017-10-06 16:57:39 -0500

    Next Up: work on camera, fix up the other activities and hopefully get the buttons to work on the main menu.
    Slowly coming together, just stay patient!

--- app/src/main/java/com/example/myleslab/seniorproject_ui2/CardView_Menu.java
+++ app/src/main/java/com/example/myleslab/seniorproject_ui2/CardView_Menu.java
@@ -1,14 +1,70 @@
 package com.example.myleslab.seniorproject_ui2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class CardView_Menu extends Activity {
+
+    CardView calendarCard, cameraCard, favoritesCard, settingsCard, weatherCard,
+            closetCard;
+    Intent nScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_card_view__menu);
+        setContentView(R.layout.fragmentcard_view);
 
+        calendarCard = (CardView) findViewById(R.id.calendar_card);
+        favoritesCard = (CardView) findViewById(R.id.favorites_card);
+        settingsCard = (CardView) findViewById(R.id.settings_card);
+        weatherCard = (CardView) findViewById(R.id.weather_card);
+        cameraCard = (CardView) findViewById(R.id.camera_card);
+        closetCard = (CardView) findViewById(R.id.closet_card);
+
+        calendarCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        favoritesCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        settingsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cameraCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nScreen = new Intent(CardView_Menu.this, cameraActivity.class);
+                startActivity(nScreen);
+            }
+        });
+
+        weatherCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        closetCard.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
