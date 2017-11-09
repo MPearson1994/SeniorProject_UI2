ebbf97d7e9bb7f42720faff94119b21f8b041d01
Myles.lab <Mpearso2@my.tnstate.edu>
2017-11-09 16:21:06 -0600

    More progress

--- app/src/main/java/com/example/myleslab/seniorproject_ui2/ViewClothing.java
+++ app/src/main/java/com/example/myleslab/seniorproject_ui2/ViewClothing.java
@@ -1,68 +1,114 @@
 package com.example.myleslab.seniorproject_ui2;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-
+import java.io.File;
 import java.util.ArrayList;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import me.relex.circleindicator.CircleIndicator;
 
 public class ViewClothing extends AppCompatActivity {
-    GalleryAdapter mAdapter;
-    RecyclerView mRecyclerView;
+    private static ViewPager mPager;
+    private static int currentPage = 0;
+    private static final File[] images = null;
+    private ArrayList<String> arrayList = new ArrayList<String>();
+    MenuItem item;
 
-    ArrayList<ImageModel> data = new ArrayList<>();
-
-    public static String IMGS[] = {
-            "https://images.unsplash.com/photo-1444090542259-0af8fa96557e?q=80&fm=jpg&w=1080&fit=max&s=4b703b77b42e067f949d14581f35019b",
-            "https://images.unsplash.com/photo-1439546743462-802cabef8e97?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1441155472722-d17942a2b76a?q=80&fm=jpg&w=1080&fit=max&s=80cb5dbcf01265bb81c5e8380e4f5cc1",
-            "https://images.unsplash.com/photo-1437651025703-2858c944e3eb?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1431538510849-b719825bf08b?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1434873740857-1bc5653afda8?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1439396087961-98bc12c21176?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1433616174899-f847df236857?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1438480478735-3234e63615bb?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1438027316524-6078d503224b?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300"
-    };
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.select_type, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_clothing);
-
-        for (int i = 0; i < IMGS.length; i++) {
-
-            ImageModel imageModel = new ImageModel();
-            imageModel.setName("Image " + i);
-            imageModel.setUrl(IMGS[i]);
-            data.add(imageModel);
-
-        }
-
-        mRecyclerView = (RecyclerView) findViewById(R.id.list);
-        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
-        mRecyclerView.setHasFixedSize(true);
-
-
-        mAdapter = new GalleryAdapter(ViewCloset.this, data);
-        mRecyclerView.setAdapter(mAdapter);
-
-        mRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this,
-                new RecyclerItemClickListener.OnItemClickListener() {
-
-                    @Override
-                    public void onItemClick(View view, int position) {
-
-                        Intent intent = new Intent(ViewCloset.this, DetailActivity.class);
-                        intent.putParcelableArrayListExtra("data", data);
-                        intent.putExtra("pos", position);
-                        startActivity(intent);
-
-                    }
-                }));
-
+        init();
     }
+
+    public String getFileName(){
+        switch (item.getItemId()) {
+            case R.id.tshirt:
+                if (item.isChecked()) item.setChecked(false);
+                else item.setChecked(true);
+                return "tshirt";
+            case R.id.pants:
+                if (item.isChecked())item.setChecked(false);
+                else item.setChecked(true);
+                return "pants";
+            case R.id.shoes:
+                if(item.isChecked())item.setChecked(false);
+                else item.setChecked(true);
+                return "shoes";
+            case R.id.longshirt:
+                if(item.isChecked())item.setChecked(false);
+                else item.setChecked(true);
+                return "longshirt";
+            case R.id.shorts:
+                if(item.isChecked())item.setChecked(false);
+                else item.setChecked(true);
+                return "shorts";
+            case R.id.other:
+                if(item.isChecked())item.setChecked(false);
+                else item.setChecked(true);
+                return "other";
+            default:
+                return "tshirt";
+        }
+    }
+    public void getFromCache() {
+        Pattern pat = Pattern.compile("[0-9]+\.jpg");
+        Matcher
+
+
+        File file= new File(android.os.Environment.getExternalStorageDirectory(),"files/pic_"+getFileName()+"_"+ );
+
+        if (file.isDirectory())
+        {
+             = file.listFiles();
+
+
+            for (int i = 0; i < listFile.length; i++)
+            {
+
+                f.add(listFile[i].getAbsolutePath());
+
+            }
+        }
+    }
+
+    private void init() {
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new PageAdapter(ViewClothing.this,arrayList));
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+
+        // Auto start of viewpager
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == images.length) {
+                    currentPage = 0;
+                }
+                mPager.setCurrentItem(currentPage++, true);
+            }
+        };
+        Timer swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 2500, 2500);
+
 }
